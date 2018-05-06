82869b8138cb809e97f405c636ac8da8dfc2a486
stuxcrystal <stuxcrystal@encode.moe>
2018-05-06 17:38:13 +0200

    Add circleci

--- yuuno/yuuno.py
+++ yuuno/yuuno.py
@@ -16,8 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import types
-from typing import Dict as TDict, Sequence, Type, TypeVar, Optional
+from typing import Sequence, Type, TypeVar, Optional
 
 from traitlets.utils.importstring import import_item
 from traitlets import Instance, List, Dict
