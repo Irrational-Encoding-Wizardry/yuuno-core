64d86f4eae236a461fbabb38fc4a92170d75d55c
stuxcrystal <stuxcrystal@encode.moe>
2018-05-08 17:09:53 +0200

    Continue writing the multi_script extension.

--- yuuno/multi_scripts/extension.py
+++ yuuno/multi_scripts/extension.py
@@ -15,13 +15,53 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from typing import Dict, Optional, Iterator, TYPE_CHECKING
+
 from yuuno.core.extension import Extension
+if TYPE_CHECKING:
+    from yuuno.multi_scripts.script import ScriptManager
 
 
-class MultiScriptExtension(object):
+class MultiScriptExtension(Extension):
+
+    _name = "MultiScript"
+    managers: Dict[str, 'ScriptManager']
 
     @classmethod
     def is_supported(self):
         return True
 
-    
+    def initialize(self):
+        self.managers = {}
+
+    def register_manager(self, name: str, manager: 'ScriptManager'):
+        """
+        Registers a new manager.
+
+        :param name:     The name of the manager.
+        :param manager:  The manager to register.
+        :return: The registered manager.
+        """
+        if name in self.managers:
+            raise ValueError("A manager with this name already exists.")
+        self.managers[name] = manager
+
+    def get_manager(self, name: str) -> Optional['ScriptManager']:
+        """
+        Returns the manager with the givern name.
+        :param name:  The name of the manager.
+        :return:      The manager that has been registered with this name.
+        """
+        return self.managers.get(name, None)
+
+    def get_manager_names(self) -> Iterator[str]:
+        """
+        Returns all currently registered managers.
+
+        :return: The currently registered manager.
+        """
+        yield from self.managers.keys()
+
+    def deinitialize(self):
+        for manager in self.managers.values():
+            manager.dispose_all()
