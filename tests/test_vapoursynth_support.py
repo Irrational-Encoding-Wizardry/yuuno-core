65e396ed367cf93a2839447a598262154117468c
stuxcrystal <stuxcrystal@encode.moe>
2018-05-11 01:13:55 +0200

    Make tests work again

--- tests/test_vapoursynth_support.py
+++ tests/test_vapoursynth_support.py
@@ -137,7 +137,7 @@ class TestVapourSynth(unittest.TestCase):
 
         self.assertEqual(len(VapourSynthClip(self.black_clip)), len(self.black_clip))
 
-        cpf = VapourSynthClip(self.black_clip_yuv444)[0].compat_frame
+        cpf = VapourSynthClip(self.black_clip_yuv444)[0].result().compat_frame
         self.assertEqual(cpf.format.id, self.vs.COMPATBGR32)
         self.assertEqual(cpf.width, self.black_clip_yuv444.width)
         self.assertEqual(cpf.height, self.black_clip_yuv444.height)
@@ -147,7 +147,7 @@ class TestVapourSynth(unittest.TestCase):
 
         self.assertEqual(len(VapourSynthFrame(self.black_clip.get_frame(0))), 1)
 
-        cpf = VapourSynthFrame(self.black_clip_yuv444.get_frame(0))[0].compat_frame
+        cpf = VapourSynthFrame(self.black_clip_yuv444.get_frame(0))[0].result().compat_frame
         self.assertEqual(cpf.format.id, self.vs.COMPATBGR32)
         self.assertEqual(cpf.width, self.black_clip_yuv444.width)
         self.assertEqual(cpf.height, self.black_clip_yuv444.height)
